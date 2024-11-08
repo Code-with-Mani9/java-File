8403a8b5832aaf4c4b69c2345bec217ac8441c89
Mani <Mani954687@gmail.com>
2024-11-08 17:47:33 +0530

    Add files via upload

---
+++ ReverseDigit.java
@@ -0,0 +1,32 @@
+package challange.challengeVVI;
+
+import java.util.Scanner;
+
+public class ReverseDigit {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter your number");
+        int num = input.nextInt();
+        int reverse = reverse(num);
+        System.out.println("Reverse number is: " + reverse);
+
+
+    }
+
+    public static int reverse(int num){
+        int newNum=0;
+      
+
+        while(num > 0){
+            int digit = num %  10;
+            newNum = newNum * 10 + digit;
+
+            num /=10;
+
+
+        }
+
+        return newNum;
+    }
+    
+}
